e707e70a6da25ba49e742cb641392c642e0ab2bb
Oscar <oscar.kennedysmith@gmail.com>
2020-06-04 14:14:31 +1000

    Defined authentication route to login user and return jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config')
 
-//Define auth function and export together  
+//Define and export Middleware function for json authentication   
 module.exports = function (req, res, next) {
     //Extract the token from the defined header that has been passed through from the CS 
     const token = req.header('x-auth-token');
@@ -12,6 +12,23 @@ module.exports = function (req, res, next) {
         return res.status(401).json({msg: "No token, authorization denied"})
     }
 
+    //Verify the token received in the header when unsigned is the same token 
+    try {
+        //Decode the token using .verify('token from header', 'secret string')
+        // and define a variable to hold the payload object 
+        const decoded = jwt.verify(token, config.get('jwtToken'));
+        
+        //If the token is not valid, an error will be caught
 
+        //If token is valid, the payload of the user ID is assigned to a property of req.'property'
+        //This will then be available for use in any protected route 
+        req.user = decoded.user;
+        //Call next at end of every middleware
+        next();
+
+    } catch (error) {
+        //401 not authorized 
+        res.status(401).json({msg: "Token not verified, authorization denied"})
+    }
 
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,16 +1,85 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-//@route   GET api/auth
-//@desc     
-//@access  Public
-router.get('/', (req, res) => {
-    try {
-        const user = User.find( )
-        
-    } catch (error) {
-        
+//Import Schemas 
+const User = require('../../models/User')
+
+//@route   POST api/auth
+//@desc    Authenticate user and get token 
+//@access  Private
+router.post(
+    '/', 
+    [check('email', 'Enter a valid email address').isEmail(),
+     check('password', 'Password is required ').exists()
+    ],
+    async (req, res) => {
+    //Check for validation errors 
+    const errors = validationResult(req);
+    //If errors kill and send response with errors array 
+    if(!errors.isEmpty()){
+        //500 bad request 
+        return res.status(500).json({ msg: errors.array() });
     }
-})
+    //Deconstruct the request body 
+    const { email, password } = req.body; 
+
+    //All database calls to be wrapped in try/catch blocks 
+    try {
+        //Check if user exists in the database 
+        //DB call use await 
+        let user = await User.findOne({email});
+        //If no user exists, respond with an error 
+        if(!user) {
+            //400 bad request 
+            return res
+                .status(400)
+                .json({msg: "Invalid credentials"})
+        }
+        //If a user exists, verify the email and password 
+        //Use bcrypt.compare to verify password string with bcrypted password in db
+        //bcrypted password is available through user object that was retrieved from db 
+        // .compare returns a promise 
+        const isMatch = await bcrypt.compare(password, user.password);
+        //If the passwords do not match
+        if(!isMatch) {
+            //400 bad request
+            return res
+                .status(400)
+                .json({msg: "Invalid Credentials"});
+        }
+
+        //JSON Web Token 
+            //Create the payload to load into the JWT
+            const payload = {
+                user: {
+                    id: user.id,
+                }
+            }
+            //Sign the token with a unique string 
+            jwt.sign(
+                //pass in the payload 
+                payload, 
+                //pass in the secret
+                config.get('jwtToken'),
+                //set expiration duration 
+                {expiresIn: 36000},
+                //define callback function to catch/throw an error or return the token to CS
+                (err, token) => {
+                    if (err) throw err;
+                //send response of jwt token 
+                    res.json(token);
+                });
+
+    } catch (error) {
+        console.error(error.message);
+        //500 internal server error 
+        res.status(500).send('Server error')
+    }
+});
 
 module.exports = router; 
