8b3c07ba8352611c5a3348ac9f145129181d5e99
Oscar <oscar.kennedysmith@gmail.com>
2020-06-04 15:32:56 +1000

    GET Profile route of current user defined

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+//Import Schemas 
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 //@route   GET api/profile
-//@desc      
-//@access  Public
-router.get('/', (req, res) => {
-    res.send("Profile route")
+//@desc     Get current user's profile 
+//@access  Private
+router.get('/me', auth, async (req, res) => {
+    //All db queries are wrapped in a try/catch block 
+    try {
+        //Check if profile for current user already exists 
+        // await got db check
+        // find one profile by the user id stored from auth header 
+        // .populate grants access to another model's properties 
+        let profile = await Profile.findOne({user: req.user.id}).populate('user', 
+        ['name', 'avatar']);
+
+        //If no profile is found 
+        if(!profile) {
+            return res
+            //500 bad request 
+                .status(500)
+                .json({msg: "No profile found"})
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+
 })
 
 module.exports = router; 
