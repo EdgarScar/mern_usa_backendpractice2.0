6b26288fd1b9f2a9bafc0a9372a3fca19bcfc795
Oscar <oscar.kennedysmith@gmail.com>
2020-06-05 15:24:46 +1000

    Initialised React App for front end

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+//Functions (arrow) rather than Classes to allow use of hooks 
+const App = () => 
+    //Fragments are like ghost elements that do not show up in the DOM 
+    <Fragment>
+        <h1>App</h1>
+    </Fragment>
+
+export default App;
